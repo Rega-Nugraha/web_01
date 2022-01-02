78dd4f220f732bbceb6179b1ad1d41378759c5a7
Reganugraha <1rega.nugraha@gmail.com>
2022-01-02 09:27:56 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+My Personal Website, built with React, Next.js, TailwindCSS, and Strapi for headlessCMS
+
+**Additional Libs**
+
+- Next-SEO
+- Next-Themes
+- Next-PWA
+- Next-MDX-Remote && mdxPrism
+- Framer Motion
+
+**TO DO**
+- [X] Syntax Highlighting
+- [ ] internalization with next-translate
+- [ ] Next Authentication
+- [ ] Guest Book
+- [ ] Comments with GitHub
+
+**More Description Soon...**
---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <meta name='theme-color' content='#171717' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icon-192.png' />
+          <link rel='shortcut icon' href='/favicon.svg' type='svg/x-icon' />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
